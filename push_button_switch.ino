8b39d67e1fa820272977c3243b28df4ec91ce3b7
Frank Obedi <frankmobedi@gmail.com>
2026-05-31 15:54:41 -0400

    Clean up code
    Removed unused variables and commented code.

--- push_button_switch.ino
+++ push_button_switch.ino
@@ -1,9 +1,5 @@
 int switchPin = 2;
-int switchVal;
-
 int redPin = 12;
-int greenPin = 4;
-
 int delayTime = 2;
 int switchOld = 1;
 int switchNew;
@@ -11,21 +7,15 @@ bool isOn = false;
 
 void setLED(bool r, bool g, bool b) {
   digitalWrite(redPin, r);
-  digitalWrite(greenPin, g);
-  // digitalWrite(bluePin, b);
 }
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(redPin, OUTPUT);
   pinMode(switchPin, INPUT);
   Serial.begin(9600);
 }
 
-
-
 void loop() {
-  // put your main code here, to run repeatedly:
   switchNew = digitalRead(switchPin);
   if(switchOld == 0 && switchNew == 1){
     isOn = !isOn;
